a3957636cafbf98c0595c41cc1b27296be99a9cd
Guilherme Lefrancois <gui.lefrancois21@gmail.com>
2020-06-06 19:51:28 -0300

    subindo+classe_teste-v1

---
+++ Eng_software/teste.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eng_software
+{
+    public class teste
+    {
+        private string nome;
+
+        public teste(string nome)
+        {
+            this.nome = nome;
+        }
+    }
+}
