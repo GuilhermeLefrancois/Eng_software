9edaeae02ba72bb2ad6cd68df625182454a67945
Guilherme Lefrancois <gui.lefrancois21@gmail.com>
2020-06-08 11:05:40 -0300

    Sunbindo_alterações_visuais

--- Eng_software/Form2.Designer.cs
+++ Eng_software/Form2.Designer.cs
@@ -30,39 +30,41 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label13 = new System.Windows.Forms.Label();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label12 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label13 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.flowLayoutPanel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button8);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.flowLayoutPanel1);
             this.panel1.Controls.Add(this.button1);
@@ -74,67 +76,88 @@
             this.panel1.Size = new System.Drawing.Size(768, 354);
             this.panel1.TabIndex = 0;
             // 
-            // label5
+            // panel2
             // 
-            this.label5.Location = new System.Drawing.Point(3, 80);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(317, 20);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "- Mestre em bater o carro";
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.label13);
+            this.panel2.Location = new System.Drawing.Point(388, 52);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(355, 220);
+            this.panel2.TabIndex = 13;
             // 
-            // label4
+            // button7
             // 
-            this.label4.Location = new System.Drawing.Point(3, 60);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(317, 20);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "- Especialista em lego";
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button7.Location = new System.Drawing.Point(0, 180);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(355, 23);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "Enviou mensagem para Julia";
+            this.button7.UseVisualStyleBackColor = true;
             // 
-            // label3
+            // button6
             // 
-            this.label3.Location = new System.Drawing.Point(3, 40);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(317, 20);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "- Programador Code.org";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.Location = new System.Drawing.Point(0, 146);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(355, 23);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "Curtu o post de Catia";
+            this.button6.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // button5
             // 
-            this.label2.Location = new System.Drawing.Point(3, 20);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(317, 20);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "- 35 anos";
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Location = new System.Drawing.Point(0, 115);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(355, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Curtiu o post de Joana";
+            this.button5.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // button4
             // 
-            this.label1.Location = new System.Drawing.Point(3, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(317, 20);
-            this.label1.TabIndex = 1;
-            this.label1.Text = " - Gabriel Borges Calheiros ";
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Location = new System.Drawing.Point(0, 86);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(355, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Cutucou Ytallu Fr4g4";
+            this.button4.UseVisualStyleBackColor = true;
             // 
-            // pictureBox1
+            // button3
             // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(19, 55);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(197, 217);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(0, 55);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(355, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Compartilhou uma publicação ";
+            this.button3.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // button2
             // 
-            this.button1.Location = new System.Drawing.Point(589, 310);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(154, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Clique aqui";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(0, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(355, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Comentou no Post de Mariana";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label13
+            // 
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(3, 3);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(317, 20);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "Ações:";
             // 
             // flowLayoutPanel1
             // 
@@ -153,6 +176,47 @@
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(317, 217);
             this.flowLayoutPanel1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = " - Gabriel Borges Calheiros ";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(3, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(317, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "- 35 anos";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(3, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(317, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "- Programador Code.org";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(3, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(317, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "- Especialista em lego";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(3, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(317, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "- Mestre em bater o carro";
             // 
             // label6
             // 
@@ -202,6 +266,27 @@
             this.label11.TabIndex = 11;
             this.label11.Text = "- Leitor de comentários";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(589, 310);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Clique aqui";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(19, 55);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 217);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // label12
             // 
             this.label12.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -211,103 +296,29 @@
             this.label12.TabIndex = 12;
             this.label12.Text = "Perfil Pessoal";
             // 
-            // panel2
+            // button8
             // 
-            this.panel2.Controls.Add(this.button7);
-            this.panel2.Controls.Add(this.button6);
-            this.panel2.Controls.Add(this.button5);
-            this.panel2.Controls.Add(this.button4);
-            this.panel2.Controls.Add(this.button3);
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Controls.Add(this.label13);
-            this.panel2.Location = new System.Drawing.Point(388, 52);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(355, 220);
-            this.panel2.TabIndex = 13;
-            // 
-            // label13
-            // 
-            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(3, 3);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(317, 20);
-            this.label13.TabIndex = 12;
-            this.label13.Text = "Ações:";
-            // 
-            // button2
-            // 
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button2.Location = new System.Drawing.Point(0, 26);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(355, 23);
-            this.button2.TabIndex = 13;
-            this.button2.Text = "Comentou no Post de Mariana";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button3.Location = new System.Drawing.Point(0, 55);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(355, 23);
-            this.button3.TabIndex = 14;
-            this.button3.Text = "Compartilhou uma publicação ";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button4.Location = new System.Drawing.Point(0, 86);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(355, 23);
-            this.button4.TabIndex = 15;
-            this.button4.Text = "Cutucou Ytallu Fr4g4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button5.Location = new System.Drawing.Point(0, 115);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(355, 23);
-            this.button5.TabIndex = 16;
-            this.button5.Text = "Curtiu o post de Joana";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button6.Location = new System.Drawing.Point(0, 146);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(355, 23);
-            this.button6.TabIndex = 17;
-            this.button6.Text = "Curtu o post de Catia";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button7.Location = new System.Drawing.Point(0, 180);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(355, 23);
-            this.button7.TabIndex = 18;
-            this.button7.Text = "Enviou mensagem para Julia";
-            this.button7.UseVisualStyleBackColor = true;
+            this.button8.Location = new System.Drawing.Point(419, 310);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(154, 23);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "Cancelar ";
+            this.button8.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackColor = System.Drawing.Color.Maroon;
             this.ClientSize = new System.Drawing.Size(797, 387);
             this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form2";
             this.Text = "Form2";
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.flowLayoutPanel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -338,5 +349,6 @@
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button8;
     }
 }
