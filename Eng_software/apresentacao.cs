bebcf975fbc650172b627427881c7cd4b6ae71b9
Gabriel <gabi_borges01@hotmail.com>
2020-06-08 10:57:42 -0300

    Adição float Salario

--- Eng_software/apresentacao.cs
+++ Eng_software/apresentacao.cs
@@ -10,6 +10,7 @@ namespace Eng_software
     {
         private string nome;
         private int idade;
+        private float salario;
 
         public apresentacao(string nome, int idade)
         {
